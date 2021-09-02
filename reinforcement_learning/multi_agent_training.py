41bdb16128598d5949021b1c9b7809ff16ef143a
Zumo09 <63203939+Zumo09@users.noreply.github.com>
2021-09-02 16:55:25 +0200

    readytogo?

--- reinforcement_learning/multi_agent_training.py
+++ reinforcement_learning/multi_agent_training.py
@@ -1,30 +1,29 @@
-from datetime import datetime
 import os
 import random
 import sys
 from argparse import ArgumentParser, Namespace
+from datetime import datetime
 from pathlib import Path
 from pprint import pprint
 
-# import psutil
-from flatland.utils.rendertools import RenderTool
 # from torch.utils.tensorboard import SummaryWriter
 import numpy as np
-# import torch
-
-from flatland.envs.rail_env import RailEnv, RailEnvActions
+from flatland.envs.malfunction_generators import malfunction_from_params, MalfunctionParameters
+from flatland.envs.observations import TreeObsForRailEnv
+from flatland.envs.predictions import ShortestPathPredictorForRailEnv
+from flatland.envs.rail_env import RailEnv
 from flatland.envs.rail_generators import sparse_rail_generator
 from flatland.envs.schedule_generators import sparse_schedule_generator
-from flatland.envs.observations import TreeObsForRailEnv
+# import psutil
+from flatland.utils.rendertools import RenderTool
 
-from flatland.envs.malfunction_generators import malfunction_from_params, MalfunctionParameters
-from flatland.envs.predictions import ShortestPathPredictorForRailEnv
+# import torch
+from reinforcement_learning.observations import CustomObservation
 
 base_dir = Path(__file__).resolve().parent.parent
 sys.path.append(str(base_dir))
 
 from utils.timer import Timer
-from utils.observation_utils import normalize_observation
 from reinforcement_learning.dddqn_policy import DDDQNPolicy
 
 # try:
@@ -101,8 +100,8 @@ def train_agent(train_params, train_env_params, eval_env_params, obs_params):
     n_episodes = train_params.n_episodes
     checkpoint_interval = train_params.checkpoint_interval
     n_eval_episodes = train_params.n_evaluation_episodes
-    restore_replay_buffer = train_params.restore_replay_buffer
-    save_replay_buffer = train_params.save_replay_buffer
+    # restore_replay_buffer = train_params.restore_replay_buffer
+    # save_replay_buffer = train_params.save_replay_buffer
 
     # Set the seeds
     random.seed(seed)
@@ -110,22 +109,22 @@ def train_agent(train_params, train_env_params, eval_env_params, obs_params):
 
     # Observation builder
     predictor = ShortestPathPredictorForRailEnv(observation_max_path_depth)
-    tree_observation = TreeObsForRailEnv(max_depth=observation_tree_depth, predictor=predictor)
+    tree_observation = CustomObservation(max_depth=observation_tree_depth, observation_radius=observation_radius,
+                                         predictor=predictor)
+    # tree_observation = TreeObsForRailEnv(max_depth=observation_tree_depth, predictor=predictor)
 
     # Setup the environments
     train_env = create_rail_env(train_env_params, tree_observation)
-    train_env.reset(regenerate_schedule=True, regenerate_rail=True)
+    obs, _ = train_env.reset(regenerate_schedule=True, regenerate_rail=True)
     eval_env = create_rail_env(eval_env_params, tree_observation)
     eval_env.reset(regenerate_schedule=True, regenerate_rail=True)
 
+    env_renderer = None
     # Setup renderer
     if train_params.render:
         env_renderer = RenderTool(train_env, gl="PGL")
 
-    # Calculate the state size given the depth of the tree observation and the number of features
-    n_features_per_node = train_env.obs_builder.observation_dim
-    n_nodes = sum([np.power(4, i) for i in range(observation_tree_depth + 1)])
-    state_size = n_features_per_node * n_nodes
+    state_size = obs[0].shape[0]
 
     # The action space of flatland is 5 discrete actions
     action_size = 5
@@ -152,39 +151,39 @@ def train_agent(train_params, train_env_params, eval_env_params, obs_params):
     # Double Dueling DQN policy
     policy = DDDQNPolicy(state_size, action_size, train_params)
 
-    # Loads existing replay buffer
-    if restore_replay_buffer:
-        try:
-            policy.load_replay_buffer(restore_replay_buffer)
-            policy.test()
-        except RuntimeError as e:
-            print("\n🛑 Could't load replay buffer, were the experiences generated using the same tree depth?")
-            print(e)
-            exit(1)
+    # # Loads existing replay buffer
+    # if restore_replay_buffer:
+    #     try:
+    #         policy.load_replay_buffer(restore_replay_buffer)
+    #         policy.test()
+    #     except RuntimeError as e:
+    #         print("\n🛑 Could't load replay buffer, were the experiences generated using the same tree depth?")
+    #         print(e)
+    #         exit(1)
+    #
+    # print("\n💾 Replay buffer status: {}/{} experiences".format(len(policy.memory.memory), train_params.buffer_size))
 
-    print("\n💾 Replay buffer status: {}/{} experiences".format(len(policy.memory.memory), train_params.buffer_size))
+    # hdd = psutil.disk_usage('/')
+    # if save_replay_buffer and (hdd.free / (2 ** 30)) < 500.0:
+    #     print("⚠️  Careful! Saving replay buffers will quickly consume a lot of disk space. You have {:.2f}gb left.".format(hdd.free / (2 ** 30)))
 
-    hdd = psutil.disk_usage('/')
-    if save_replay_buffer and (hdd.free / (2 ** 30)) < 500.0:
-        print("⚠️  Careful! Saving replay buffers will quickly consume a lot of disk space. You have {:.2f}gb left.".format(hdd.free / (2 ** 30)))
-
-    # TensorBoard writer
-    writer = SummaryWriter()
-    writer.add_hparams(vars(train_params), {})
-    writer.add_hparams(vars(train_env_params), {})
-    writer.add_hparams(vars(obs_params), {})
+    # # TensorBoard writer
+    # writer = SummaryWriter()
+    # writer.add_hparams(vars(train_params), {})
+    # writer.add_hparams(vars(train_env_params), {})
+    # writer.add_hparams(vars(obs_params), {})
 
     training_timer = Timer()
     training_timer.start()
 
-    print("\n🚉 Training {} trains on {}x{} grid for {} episodes, evaluating on {} episodes every {} episodes. Training id '{}'.\n".format(
-        train_env.get_num_agents(),
-        x_dim, y_dim,
-        n_episodes,
-        n_eval_episodes,
-        checkpoint_interval,
-        training_id
-    ))
+    print("\n🚉 Training {} trains on {}x{} grid for {} episodes, evaluating on {} episodes every {} episodes. "
+          "Training id '{}'.\n".format(
+            train_env.get_num_agents(),
+            x_dim, y_dim,
+            n_episodes,
+            n_eval_episodes,
+            checkpoint_interval,
+            training_id))
 
     for episode_idx in range(n_episodes + 1):
         step_timer = Timer()
@@ -202,13 +201,12 @@ def train_agent(train_params, train_env_params, eval_env_params, obs_params):
             env_renderer.set_new_rail()
 
         score = 0
-        nb_steps = 0
         actions_taken = []
 
         # Build initial agent-specific observations
         for agent in train_env.get_agent_handles():
-            if obs[agent]:
-                agent_obs[agent] = normalize_observation(obs[agent], observation_tree_depth, observation_radius=observation_radius)
+            if obs[agent] is not None:
+                agent_obs[agent] = obs[agent]
                 agent_prev_obs[agent] = agent_obs[agent].copy()
 
         # Run episode
@@ -255,9 +253,9 @@ def train_agent(train_params, train_env_params, eval_env_params, obs_params):
                     agent_prev_action[agent] = action_dict[agent]
 
                 # Preprocess the new observations
-                if next_obs[agent]:
+                if next_obs[agent] is not None:
                     preproc_timer.start()
-                    agent_obs[agent] = normalize_observation(next_obs[agent], observation_tree_depth, observation_radius=observation_radius)
+                    agent_obs[agent] = next_obs[agent]
                     preproc_timer.end()
 
                 score += all_rewards[agent]
@@ -283,10 +281,10 @@ def train_agent(train_params, train_env_params, eval_env_params, obs_params):
 
         # Print logs
         if episode_idx % checkpoint_interval == 0:
-            torch.save(policy.qnetwork_local, './checkpoints/' + training_id + '-' + str(episode_idx) + '.pth')
+            policy.save('./checkpoints/' + training_id + '-' + str(episode_idx))
 
-            if save_replay_buffer:
-                policy.save_replay_buffer('./replay_buffers/' + training_id + '-' + str(episode_idx) + '.pkl')
+            # if save_replay_buffer:
+            #     policy.save_replay_buffer('./replay_buffers/' + training_id + '-' + str(episode_idx) + '.pkl')
 
             if train_params.render:
                 env_renderer.close_window()
@@ -312,48 +310,48 @@ def train_agent(train_params, train_env_params, eval_env_params, obs_params):
         if episode_idx % checkpoint_interval == 0 and n_eval_episodes > 0:
             scores, completions, nb_steps_eval = eval_policy(eval_env, policy, train_params, obs_params)
 
-            writer.add_scalar("evaluation/scores_min", np.min(scores), episode_idx)
-            writer.add_scalar("evaluation/scores_max", np.max(scores), episode_idx)
-            writer.add_scalar("evaluation/scores_mean", np.mean(scores), episode_idx)
-            writer.add_scalar("evaluation/scores_std", np.std(scores), episode_idx)
-            writer.add_histogram("evaluation/scores", np.array(scores), episode_idx)
-            writer.add_scalar("evaluation/completions_min", np.min(completions), episode_idx)
-            writer.add_scalar("evaluation/completions_max", np.max(completions), episode_idx)
-            writer.add_scalar("evaluation/completions_mean", np.mean(completions), episode_idx)
-            writer.add_scalar("evaluation/completions_std", np.std(completions), episode_idx)
-            writer.add_histogram("evaluation/completions", np.array(completions), episode_idx)
-            writer.add_scalar("evaluation/nb_steps_min", np.min(nb_steps_eval), episode_idx)
-            writer.add_scalar("evaluation/nb_steps_max", np.max(nb_steps_eval), episode_idx)
-            writer.add_scalar("evaluation/nb_steps_mean", np.mean(nb_steps_eval), episode_idx)
-            writer.add_scalar("evaluation/nb_steps_std", np.std(nb_steps_eval), episode_idx)
-            writer.add_histogram("evaluation/nb_steps", np.array(nb_steps_eval), episode_idx)
-
-            smoothing = 0.9
-            smoothed_eval_normalized_score = smoothed_eval_normalized_score * smoothing + np.mean(scores) * (1.0 - smoothing)
-            smoothed_eval_completion = smoothed_eval_completion * smoothing + np.mean(completions) * (1.0 - smoothing)
-            writer.add_scalar("evaluation/smoothed_score", smoothed_eval_normalized_score, episode_idx)
-            writer.add_scalar("evaluation/smoothed_completion", smoothed_eval_completion, episode_idx)
-
-        # Save logs to tensorboard
-        writer.add_scalar("training/score", normalized_score, episode_idx)
-        writer.add_scalar("training/smoothed_score", smoothed_normalized_score, episode_idx)
-        writer.add_scalar("training/completion", np.mean(completion), episode_idx)
-        writer.add_scalar("training/smoothed_completion", np.mean(smoothed_completion), episode_idx)
-        writer.add_scalar("training/nb_steps", nb_steps, episode_idx)
-        writer.add_histogram("actions/distribution", np.array(actions_taken), episode_idx)
-        writer.add_scalar("actions/nothing", action_probs[RailEnvActions.DO_NOTHING], episode_idx)
-        writer.add_scalar("actions/left", action_probs[RailEnvActions.MOVE_LEFT], episode_idx)
-        writer.add_scalar("actions/forward", action_probs[RailEnvActions.MOVE_FORWARD], episode_idx)
-        writer.add_scalar("actions/right", action_probs[RailEnvActions.MOVE_RIGHT], episode_idx)
-        writer.add_scalar("actions/stop", action_probs[RailEnvActions.STOP_MOVING], episode_idx)
-        writer.add_scalar("training/epsilon", eps_start, episode_idx)
-        writer.add_scalar("training/buffer_size", len(policy.memory), episode_idx)
-        writer.add_scalar("training/loss", policy.loss, episode_idx)
-        writer.add_scalar("timer/reset", reset_timer.get(), episode_idx)
-        writer.add_scalar("timer/step", step_timer.get(), episode_idx)
-        writer.add_scalar("timer/learn", learn_timer.get(), episode_idx)
-        writer.add_scalar("timer/preproc", preproc_timer.get(), episode_idx)
-        writer.add_scalar("timer/total", training_timer.get_current(), episode_idx)
+        #     writer.add_scalar("evaluation/scores_min", np.min(scores), episode_idx)
+        #     writer.add_scalar("evaluation/scores_max", np.max(scores), episode_idx)
+        #     writer.add_scalar("evaluation/scores_mean", np.mean(scores), episode_idx)
+        #     writer.add_scalar("evaluation/scores_std", np.std(scores), episode_idx)
+        #     writer.add_histogram("evaluation/scores", np.array(scores), episode_idx)
+        #     writer.add_scalar("evaluation/completions_min", np.min(completions), episode_idx)
+        #     writer.add_scalar("evaluation/completions_max", np.max(completions), episode_idx)
+        #     writer.add_scalar("evaluation/completions_mean", np.mean(completions), episode_idx)
+        #     writer.add_scalar("evaluation/completions_std", np.std(completions), episode_idx)
+        #     writer.add_histogram("evaluation/completions", np.array(completions), episode_idx)
+        #     writer.add_scalar("evaluation/nb_steps_min", np.min(nb_steps_eval), episode_idx)
+        #     writer.add_scalar("evaluation/nb_steps_max", np.max(nb_steps_eval), episode_idx)
+        #     writer.add_scalar("evaluation/nb_steps_mean", np.mean(nb_steps_eval), episode_idx)
+        #     writer.add_scalar("evaluation/nb_steps_std", np.std(nb_steps_eval), episode_idx)
+        #     writer.add_histogram("evaluation/nb_steps", np.array(nb_steps_eval), episode_idx)
+        #
+        #     smoothing = 0.9
+        #     smoothed_eval_normalized_score = smoothed_eval_normalized_score * smoothing + np.mean(scores) * (1.0 - smoothing)
+        #     smoothed_eval_completion = smoothed_eval_completion * smoothing + np.mean(completions) * (1.0 - smoothing)
+        #     writer.add_scalar("evaluation/smoothed_score", smoothed_eval_normalized_score, episode_idx)
+        #     writer.add_scalar("evaluation/smoothed_completion", smoothed_eval_completion, episode_idx)
+        #
+        # # Save logs to tensorboard
+        # writer.add_scalar("training/score", normalized_score, episode_idx)
+        # writer.add_scalar("training/smoothed_score", smoothed_normalized_score, episode_idx)
+        # writer.add_scalar("training/completion", np.mean(completion), episode_idx)
+        # writer.add_scalar("training/smoothed_completion", np.mean(smoothed_completion), episode_idx)
+        # writer.add_scalar("training/nb_steps", nb_steps, episode_idx)
+        # writer.add_histogram("actions/distribution", np.array(actions_taken), episode_idx)
+        # writer.add_scalar("actions/nothing", action_probs[RailEnvActions.DO_NOTHING], episode_idx)
+        # writer.add_scalar("actions/left", action_probs[RailEnvActions.MOVE_LEFT], episode_idx)
+        # writer.add_scalar("actions/forward", action_probs[RailEnvActions.MOVE_FORWARD], episode_idx)
+        # writer.add_scalar("actions/right", action_probs[RailEnvActions.MOVE_RIGHT], episode_idx)
+        # writer.add_scalar("actions/stop", action_probs[RailEnvActions.STOP_MOVING], episode_idx)
+        # writer.add_scalar("training/epsilon", eps_start, episode_idx)
+        # writer.add_scalar("training/buffer_size", len(policy.memory), episode_idx)
+        # writer.add_scalar("training/loss", policy.loss, episode_idx)
+        # writer.add_scalar("timer/reset", reset_timer.get(), episode_idx)
+        # writer.add_scalar("timer/step", step_timer.get(), episode_idx)
+        # writer.add_scalar("timer/learn", learn_timer.get(), episode_idx)
+        # writer.add_scalar("timer/preproc", preproc_timer.get(), episode_idx)
+        # writer.add_scalar("timer/total", training_timer.get_current(), episode_idx)
 
 
 def format_action_prob(action_probs):
@@ -388,8 +386,8 @@ def eval_policy(env, policy, train_params, obs_params):
 
         for step in range(max_steps - 1):
             for agent in env.get_agent_handles():
-                if obs[agent]:
-                    agent_obs[agent] = normalize_observation(obs[agent], tree_depth=tree_depth, observation_radius=observation_radius)
+                if obs[agent] is not None:
+                    agent_obs[agent] = obs[agent]
 
                 action = 0
                 if info['action_required'][agent]:
